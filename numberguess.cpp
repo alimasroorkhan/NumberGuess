cd2e798dd45d628e45d3cf9971502622e12b747f
Ali Khan <148343873+alimasroorkhan@users.noreply.github.com>
2024-05-07 11:40:16 +0500

    Update numberguess.cpp
    better execution.

--- numberguess.cpp
+++ numberguess.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
+#include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-void game()
-{
-	srand(static_cast<unsigned int>(time(0)));
-	int secretnum = rand() % 25 + 1;
+void playGame() {
+    srand(static_cast<unsigned int>(time(0)));
+    int secretNum = rand() % 25 + 1;
 
-	int guess = 0;
-	int maxattempts = 5;
-	int attempt = 0;
+    int maxAttempts = 5;
+    int attempt = 0;
+    int guess;
 
-	while (attempt < maxattempts)
-	{
-		cout << "Can you guess the number between 1 & 25?\n";
-		cin >> guess;
+    while (attempt < maxAttempts) {
+        cout << "Guess the number between 1 & 25: ";
+        cin >> guess;
 
-		if (guess > secretnum)
-		{
-			cout << "Too high! try again.\n";
-		}
-		else if (guess < secretnum)
-		{
-			cout << "Too low! try again.\n";
-		}
-		else if (guess == secretnum)
-		{
-			cout << "Bingo! you have guessed the secret number!\n";
-			break;
-		}
-		attempt++;
-		cout << "Attempts remaining : " << maxattempts - attempt << endl;
-	}
-	if (attempt == maxattempts)
-	{
-		cout << "Sorry, you've run out of attempts. The secret number was: " << secretnum << endl;
-	}
+        if (guess == secretNum) {
+            cout << "Congratulations! You guessed the secret number.\n";
+            return;
+        }
+        else if (guess < secretNum) {
+            cout << "Too low! Try again.\n";
+        }
+        else {
+            cout << "Too high! Try again.\n";
+        }
+
+        attempt++;
+        cout << "Attempts remaining: " << maxAttempts - attempt << endl;
+    }
+
+    cout << "Sorry, you've run out of attempts. The secret number was: " << secretNum << endl;
 }
 
-void main()
-{
-	char playAgain = 'y';
-	while (playAgain == 'y' || playAgain == 'Y')
-	{
-		game();
-		cout << "Do you want to play again? (y/n): ";
-		cin >> playAgain;
-	}
+int main() {
+    char playAgain = 'y';
 
-	cout << "Thanks for playing!\n";
+    while (playAgain == 'y' || playAgain == 'Y') {
+        playGame();
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+    }
 
+    cout << "Thanks for playing!\n";
+
+    return 0;
 }
